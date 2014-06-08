2854582630c78d68c352041e717681039ed6ba1d
Demirhan Aydin <demirhanaydin@gmail.com>
2014-06-08 13:42:10 +0300

    added popular_tags scope fixed url path

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,7 +3,8 @@ class Recipe < ActiveRecord::Base
 
   acts_as_taggable
 
-  scope :recent, -> { order('id DESC').limit(4) }
+  scope :recent, -> { order('id DESC').limit(5) }
+  scope :popular_tags, -> {tag_counts_on(:tags).limit(5).order('taggings_count DESC')}
 
   def short_desc
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vulputate eros nec odio egestas in dictum nisi vehicula. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Suspendisse porttitor luctus imperdiet. <a href=\"#\">Praesent ultricies</a> enim ac ipsum aliquet pellentesque. Nullam justo nunc, dignissim at convallis posuere, sodales eu orci. Duis a risus sed dolor placerat semper quis in urna. Ut <strong>commodo ullamcorper risus nec</strong> viverra, dignissim eget est."
