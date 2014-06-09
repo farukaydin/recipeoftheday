95968d95478084447159d34e5f6e1fd345c6a49b
Demirhan Aydin <demirhanaydin@gmail.com>
2014-06-09 07:28:14 +0300

    updated short_desc

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -15,6 +15,6 @@ class Recipe < ActiveRecord::Base
   validates :title, :body, :status, presence: true
 
   def short_desc
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vulputate eros nec odio egestas in dictum nisi vehicula. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Suspendisse porttitor luctus imperdiet. <a href=\"#\">Praesent ultricies</a> enim ac ipsum aliquet pellentesque. Nullam justo nunc, dignissim at convallis posuere, sodales eu orci. Duis a risus sed dolor placerat semper quis in urna. Ut <strong>commodo ullamcorper risus nec</strong> viverra, dignissim eget est."
+    self.body[0..500]
   end
 end
